c7915a3065a5af4450ace4f82bd4488adb5932d2
Viktor Shishkin <viktorshishkin@MacBook-Air-Viktor.local>
2022-09-03 20:21:58 +0300

    Добавили задачу 2-ю задачу

---
+++ Zadanie02/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Input first number ");
+string input1 = Console.ReadLine();
+int number1 = int.Parse(input1);
+Console.WriteLine("Input second number ");
+string input2 = Console.ReadLine();
+int number2 = int.Parse(input2);
+if (number1 == (number2*number2))
+{
+   Console.WriteLine("First number is square second numer");
+}
+if (number2 == (number1*number1))
+{
+   Console.WriteLine("Second number is square first numer"); 
+}
+if ((number1*number1) != (number2)) 
