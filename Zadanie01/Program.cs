dea03d784f45cd457c94e026b653f1cb46d140a2
Viktor Shishkin <viktorshishkin@MacBook-Air-Viktor.local>
2022-09-03 19:50:01 +0300

    Добавили задачу 1-ю задачу

---
+++ Zadanie01/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Input number ");
+string input = Console.ReadLine();
+int number = int.Parse (input);
+int squaer = number*number;
+Console.WriteLine($"Square = {squaer}");
+
+
