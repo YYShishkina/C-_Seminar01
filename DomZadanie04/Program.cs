4c7d671b8ac27fabb4c39d0a39fb44d478360cca
Viktor Shishkin <viktorshishkin@MacBook-Air-Viktor.local>
2022-09-04 13:23:03 +0300

    Добавлено решение 4-й домашней задачи

---
+++ DomZadanie04/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Input number ");
+string number = Console.ReadLine();
+int n = int.Parse(number);
+for (int i = 1; i <= n; i++)
+{
+    int ostatok = i % 2;
+    if (ostatok == 0)
+    {
+        Console.Write($"{i}, ");
+    }
+}
