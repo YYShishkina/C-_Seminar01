11345f8294b916ade5b3e35771997e551ce81632
Viktor Shishkin <viktorshishkin@MacBook-Air-Viktor.local>
2022-09-04 13:07:06 +0300

    Добавлено задание 3-й домашей задачи

---
+++ DomZadanie03/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Input number ");
+string number1 = Console.ReadLine();
+int n1 = int.Parse(number1);
+int ostatok = n1 % 2;
+if (ostatok == 0)
+{
+   Console.WriteLine($"{n1} Chetnoe"); 
+}
+else
+{
+    Console.WriteLine($"{n1} NeChetnoe"); 
+}
