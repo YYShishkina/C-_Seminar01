66508a1b087869d5ffa7e3c9ee31168ef6074eec
Viktor Shishkin <viktorshishkin@MacBook-Air-Viktor.local>
2022-09-04 12:10:23 +0300

    Добавили Задание 5

---
+++ Zadanie05/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Input number ");
+string number = Console.ReadLine();
+int n = int.Parse(number);
+int MlRazr = n % 10;
+Console.WriteLine($"Output rezult {MlRazr}");
+
