b1c7d7b11d4e7b0fe2bccf16c0c6baf6850d5714
Viktor Shishkin <viktorshishkin@MacBook-Air-Viktor.local>
2022-09-04 11:51:53 +0300

    Добавили 3-ю задачу

---
+++ Zadanie03/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Input daynumer ");
+string input = Console.ReadLine();
+int daynumber = int.Parse(input);
+//Console.WriteLine("Hello, World!");
+if (daynumber == 1)
+{
+    Console.WriteLine("monday");
+}
+else if (daynumber == 2)
+{
+    Console.WriteLine("tuesday");
+}
+else if (daynumber == 3)
+{
+    Console.WriteLine("wednesday");
+}
+else if (daynumber == 4)
+{
+    Console.WriteLine("thursday");
+}
+else if (daynumber == 5)
+{
+    Console.WriteLine("friday");
+}
+else if (daynumber == 6)
+{
+    Console.WriteLine("saturday");
+}
+else if (daynumber == 7)
+{
+    Console.WriteLine("sunday");
+}
