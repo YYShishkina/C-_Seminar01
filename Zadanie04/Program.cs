6f9f270f1de5b50cd1ccb0e9ed377181144c306d
Viktor Shishkin <viktorshishkin@MacBook-Air-Viktor.local>
2022-09-04 12:02:25 +0300

    Добавили 4-е задание

---
+++ Zadanie04/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Input number ");
+string number = Console.ReadLine();
+int n = int.Parse(number);
+for (int i = -n; i <= n; i++)
+{
+    Console.WriteLine(i);
+}
