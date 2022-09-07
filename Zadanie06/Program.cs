c665dac1ff5d783053aff6e9d87eace6a05cdc9a
Viktor Shishkin <viktorshishkin@MacBook-Air-Viktor.local>
2022-09-07 13:49:26 +0300

    Добавлена задача сравнения цифр в двухзначном числе

---
+++ Zadanie06/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int number = new Random().Next (10,100);
+int fig1 = number/10;
+int fig2 = number%10;
+Console.WriteLine($"{number}, {fig1}, {fig2}");
+if (fig1>fig2)
+{
+    Console.WriteLine($"Bigger {fig1}");
+}
+else 
+{
+    Console.WriteLine($"Bigger {fig2}");
+}
