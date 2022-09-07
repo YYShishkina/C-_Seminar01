6cf52b3698f26e86ab05eec20807e4329cb6a6ad
Viktor Shishkin <viktorshishkin@MacBook-Air-Viktor.local>
2022-09-07 14:00:07 +0300

    Добавлено задание которое убирает вторую цифру трехзначного числа

---
+++ Zadanie07/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int number = new Random().Next (100,1000);
+int fig1 = number/100;
+int fig3 = number%10;
+Console.WriteLine($"{number}, {fig1}{fig3}");
