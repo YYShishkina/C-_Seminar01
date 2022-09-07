f45f78c3c98268209bae48d0c50401e08a961c58
Viktor Shishkin <viktorshishkin@MacBook-Air-Viktor.local>
2022-09-07 21:17:42 +0300

    Добавили задание, котрое проверяет квадрат

---
+++ Zadanie09/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Hello, World!");
+string input1 = Console.ReadLine();
+int number1 = int.Parse (input1);
+string input2 = Console.ReadLine();
+int number2 = int.Parse (input2);
+if ((number1*number1==number2)||(number2*number2==number1))
+{
+   Console.WriteLine($"Yes"); 
+}
+else
+{
+    Console.WriteLine($"No");
+} 
