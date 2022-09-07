b6a3526b08fd7df53a0af3209741b61b8a5319b3
Viktor Shishkin <viktorshishkin@MacBook-Air-Viktor.local>
2022-09-07 14:11:45 +0300

    Добавлена задача проверки на кратность или вывола остатка

---
+++ Zadanie08/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string input1 = Console.ReadLine();
+int number1 = int.Parse (input1);
+string input2 = Console.ReadLine();
+int number2 = int.Parse (input2);
+if (number1%number2==0)
+{
+   Console.WriteLine($"{number2} multiple {number1}"); 
+}
+else
+{
+    Console.WriteLine($"{number1%number2}"); 
+}
+
+
