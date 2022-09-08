74058a27de1a817be91f7070bfe023649bd027bc
Viktor Shishkin <viktorshishkin@MacBook-Air-Viktor.local>
2022-09-08 06:07:27 +0300

    Добавлено решение задачи, которая выводит вторую цифру в трехзначном числе

---
+++ DomZadanie05/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 → 5
+//782 → 8 
+//918 → 1
+
+Console.WriteLine("Hello, World!");
+int number = new Random().Next (100,1000);
+Console.WriteLine($"{number}");
+Console.WriteLine($"{(number/10)%10}");
