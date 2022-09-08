844a728a6dbba8d3492a42c527d99ab12ca15c26
Viktor Shishkin <viktorshishkin@MacBook-Air-Viktor.local>
2022-09-08 12:42:52 +0300

    Добавлено решение, задачи по выводу 3-го разряда в числе

---
+++ DomZadanie06/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 → 5
+//78 → третьей цифры нет
+//32679 → 6
+Console.WriteLine("Input number");
+//int number = new Random().Next (0,1000000);
+string n1 = Console.ReadLine(); //Ввод любого числа
+int number = int.Parse(n1);
+//Console.WriteLine($"{number}");
+
+int celaychast = number;
+int kolrazr = 0;
+if (number < 100) //Анализ есть ли вч числе 3 разрада
+{
+    Console.WriteLine($"{number}, doesn't have third digit");
+    celaychast = 0;
+}
+else
+{
+    while (celaychast>0) //Анализ количества разрядов
+    {
+        celaychast = celaychast/10;
+        kolrazr=kolrazr+1;
+    }
+    celaychast = number;
+    //Console. WriteLine($"{celaychast}, {kolrazr}");
+    while (kolrazr>3) //Определение 3-го разряда
+    {
+        celaychast = celaychast/10;
+        kolrazr=kolrazr-1;
+    }
+    //Console. WriteLine($"{celaychast}");
+    Console. WriteLine($"Third digit in number is {celaychast%10}");
+}
