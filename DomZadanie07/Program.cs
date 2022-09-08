18bdf9f1375031d29ad5b0f8a946f50085fc7ef9
Viktor Shishkin <viktorshishkin@MacBook-Air-Viktor.local>
2022-09-08 12:53:33 +0300

    Добавлено решение задания,которое определяет является ли день недели рабочим или выходным

---
+++ DomZadanie07/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+int dayn = new Random().Next (1,8);
+Console.WriteLine($"Input day number, {dayn}");
+if (dayn<=5)
+{
+    Console.WriteLine($"{dayn} is a working day");
+}
+else 
+{
+    Console.WriteLine($"{dayn} is a day off");
+}
