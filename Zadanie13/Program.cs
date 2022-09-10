4a6c2320345a2059ba85b0c3deb3e870f243e68b
Viktor Shishkin <viktorshishkin@MacBook-Air-Viktor.local>
2022-09-10 22:08:09 +0300

    Добавлена задача по выводу списка квадратов от 1 до N

---
+++ Zadanie13/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+string input1 = Console.ReadLine()!;
+int n = int.Parse (input1);
+//string input2 = Console.ReadLine();
+//int number2 = int.Parse (input2);
+
+for (int i = 1; i <= n; i++)
+{
+    Console.Write($"{i*i},");
+}
