2993339b3b154742c021413b9259f652861b245b
Viktor Shishkin <viktorshishkin@MacBook-Air-Viktor.local>
2022-09-10 16:27:34 +0300

    Добавлено задание про определение четврети по координатам

---
+++ Zadanie10/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine($"Введите Х и Н, ");
+string A = Console.ReadLine()!;
+string[] coordinate = A.Split(" ");
+int x = int.Parse (coordinate[0]);
+int y = int.Parse (coordinate[1]);
+
+if (x>0 && y>0)
+{
+    Console.WriteLine($"Точка А с координатами ({x},{y}), расположена в 1-й четверти");
+}
+else if (x<0 && y>0)
+{
+    Console.WriteLine($"Точка А с координатами ({x},{y}), расположена в 2-й четверти");
+}
+else if (x<0 && y<0)
+{
+    Console.WriteLine($"Точка А с координатами ({x},{y}), расположена в 3-й четверти");
+}
+else if (x>0 && y<0)
+{
+    Console.WriteLine($"Точка А с координатами ({x},{y}), расположена в 4-й четверти");
+}
+else 
+{
+     Console.WriteLine($"Точка А с некорректными координатами");
+}
