35573d3eb72abeb32e0edf9594bc61da1f62c3d7
Viktor Shishkin <viktorshishkin@MacBook-Air-Viktor.local>
2022-09-11 13:58:07 +0300

    Добавлено решение задачи, по определению расстояния от начала координат до точки А

---
+++ DomDopZadanie05/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write($"Введите координаты точки А ");
+string A = Console.ReadLine()!;
+string[] coordinate = A.Split(" ");
+double distance = Math.Sqrt(Math.Pow(int.Parse (coordinate[0]),2)+ Math.Pow(int.Parse (coordinate[1]),2));
+Console.WriteLine($"Расстояние от начала координат до точки А {distance:F3}");
