fdc4c0b01f6558277d9aaa75e9eabffad942c192
Viktor Shishkin <viktorshishkin@MacBook-Air-Viktor.local>
2022-09-11 13:09:15 +0300

    Добавлено задание, котрое опраделяет какое из введнных чисел больше

---
+++ DomDopZadanie02/Program.cs
@@ -0,0 +1,20 @@
+﻿// 2. Пользователь вводит x и y. Программа печатает, какое отношение верно:
+//x < y
+//x = y
+////x > y
+//Например: 3, 7 → 3 < 7
+Console.Write($"Введите значения Х и Y ");
+string A = Console.ReadLine()!;
+string[] coordinate = A.Split(" ");
+if (int.Parse (coordinate[0]) > int.Parse (coordinate[1]))
+{
+    Console.WriteLine($"x>y");
+}
+else if (int.Parse (coordinate[0])== int.Parse (coordinate[1]))
+{
+    Console.WriteLine($"x=y");
+}
+else 
+{
+    Console.WriteLine($"x<y");
+} 
