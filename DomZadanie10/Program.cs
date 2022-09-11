535aee9635e1c9f17e92b4e2105cd2a887c4f695
Viktor Shishkin <viktorshishkin@MacBook-Air-Viktor.local>
2022-09-11 10:47:41 +0300

    Добавлена задача на вывод кубов от 1 до N

---
+++ DomZadanie10/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 4, 9. 
+//5 -> 1, 8, 27, 64, 125
+
+string input1 = Console.ReadLine()!;
+int n = int.Parse (input1);
+for (int i = 1; i <= n; i++)
+{
+    Console.Write($"{i*i*i},");   
+}
+Console.WriteLine($" ");
