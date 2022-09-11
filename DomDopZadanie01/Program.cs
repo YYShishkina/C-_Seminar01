9d1d84891787b15dd2c796b4ac93f4c5f12463cd
Viktor Shishkin <viktorshishkin@MacBook-Air-Viktor.local>
2022-09-11 13:00:00 +0300

    Добавлено дополнительное задание, которое формирует сумму квадратов двух введенных чиел, убрали лишнюю переменную

---
+++ DomDopZadanie01/Program.cs
@@ -0,0 +1,10 @@
+﻿// 1. Пользователь вводит x и y. Программа печатает, чему равно x² + y².
+//Например: 1, 2 → 5
+
+Console.Write($"Введите координаты значени Х и Y ");
+string A = Console.ReadLine()!;
+string[] coordinate = A.Split(" ");
+int x = int.Parse (coordinate[0]);
+int y = int.Parse (coordinate[1]);
+double distance = (Math.Pow(x,2) + Math.Pow(y, 2));
+Console.WriteLine($"Сумма квадратов {distance}");
