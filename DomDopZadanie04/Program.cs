936d266eef1f7087982f3d8db6aaf0b09e797668
Viktor Shishkin <viktorshishkin@MacBook-Air-Viktor.local>
2022-09-11 13:49:09 +0300

    Добавлено решение задания по определению предоставления скидки по возрасту покупателя

---
+++ DomDopZadanie04/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write($"Input client's age ");
+string A = Console.ReadLine()!;
+string[] coordinate = A.Split(" ");
+if (
+    ((int.Parse (coordinate[0]) < 18) && (int.Parse (coordinate[0]) > 0))|| (int.Parse (coordinate[0]) > 65))
+{
+   Console.WriteLine($"Client has discount");   
+}
+else 
+{
+    Console.WriteLine($"Client has not discount");
+} 
