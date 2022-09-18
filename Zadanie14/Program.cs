8ac1d04bdebad9d8f04a4f072af9c1b72a6f21a7
Viktor Shishkin <viktorshishkin@MacBook-Air-Viktor.local>
2022-09-18 11:27:36 +0300

    Добавлено решение определение количество разрядов в числе с использованием метода

---
+++ Zadanie14/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+void DischargeCounter (int number )
+{
+    int counter = 0;
+    while (number !=0 )
+    {
+        number = number/10;
+        counter++;
+    }
+    Console.Write($"Number of digits --> {counter}");
+    Console.WriteLine();
+}
+
+Console.Write("input number --> ");
+int number = int.Parse(Console.ReadLine()!);
+DischargeCounter(number);
