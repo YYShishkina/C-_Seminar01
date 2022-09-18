b3153f12760d0dd68efd47a41f0fc758faf86eb5
Viktor Shishkin <viktorshishkin@MacBook-Air-Viktor.local>
2022-09-18 12:09:17 +0300

    Добавлена задача, по формирования массива из 8 случайных чисел от 0 до 1

---
+++ Zadanie16/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+//[1,0,1,1,0,1,0,0]
+
+
+void CreateArray ()
+{
+    int[] array = {1,1,1,1,1,1,1,1};
+    cdConsole.Write($"Array --> ");
+    for (int i =0; i<8; i++)
+    {
+        array[i]=new Random().Next(0,2);
+        Console.Write($"{array[i]},");
+    }
+}
+
+CreateArray();
+Console.WriteLine();
