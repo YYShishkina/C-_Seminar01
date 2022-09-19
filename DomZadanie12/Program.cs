40f1aab3aef17e17277251bde9f467243b956e1d
Viktor Shishkin <viktorshishkin@MacBook-Air-Viktor.local>
2022-09-19 07:04:50 +0300

    Добавлена задача по определению суммы цифрв числе

---
+++ DomZadanie12/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+void SumDigitsNumber (int number )
+{
+    int counter = 0;
+    int remainder = number;
+    int sumdigits = 0;
+    while (number !=0 )
+    {
+        number = number/10;
+        counter++;
+        sumdigits=sumdigits+remainder%10;
+        remainder=number;
+    }
+    Console.Write($"Sum of digits --> {sumdigits}");
+    Console.WriteLine();
+}
+
+Console.Write("input number --> ");
+int number = int.Parse(Console.ReadLine()!);
+SumDigitsNumber(number);
