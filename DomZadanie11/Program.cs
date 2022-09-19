09f2e19fcb8bec69ea5244c7b7f26c9459e43824
Viktor Shishkin <viktorshishkin@MacBook-Air-Viktor.local>
2022-09-19 06:52:17 +0300

    Добавлена зада возведение А в стпень B

---
+++ DomZadanie11/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+void Exponentiation (int NumberA, int NumberB)
+{
+    int result = 1;
+    for (int i = 1; i <= NumberB; i++)
+    {
+        result = result*NumberA;
+    }
+    Console.Write ($"{NumberA} to the power of a {NumberB} is --> {result}");
+    Console.WriteLine();
+}
+    
+Console.Write("input NumberA --> ");
+int nA = int.Parse (Console.ReadLine()!);
+Console.Write("input NumberB --> ");
+int nB = int.Parse (Console.ReadLine()!);
+Exponentiation(nA, nB);
