5cd64c7d4ae90629eeb42beb5b369fb69dbd5700
Viktor Shishkin <viktorshishkin@MacBook-Air-Viktor.local>
2022-10-03 19:33:36 +0300

    Добавлено решение задачи 67

---
+++ Zadanie38/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+
+Console.Write("Input N --> ");
+int elementN = int.Parse(Console.ReadLine()!);
+int sumNumbers=FindSumNumbers(elementN);
+Console.WriteLine($"{sumNumbers}");
+Console.WriteLine();
+
+int FindSumNumbers(int numberN)
+{
+    
+    if (numberN==0)
+    {
+        return 0;
+    }
+    return numberN%10+FindSumNumbers(numberN/10);
+}
